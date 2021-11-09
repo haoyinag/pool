51c1c648f2e0a09acf2d2dae4ac022784be57a3d
郁南 <haoyinag0@hotmail.com>
2021-11-09 08:09:37 +0800

    docker init

--- docker/index.js
+++ docker/index.js
@@ -2,8 +2,15 @@
  * @Author: 郁南
  * @LastEditors: 郁南
  * @Date: 2021-11-08 07:46:03
- * @LastEditTime: 2021-11-08 07:46:06
- * @FilePath: /pool/docker/index.js
+ * @LastEditTime: 2021-11-08 08:00:28
+ * @FilePath: /docker/index.js
  * @Description: 
  */
-yarn 
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+   ctx.body = 'Hello docker , My name is yunan';
+});
+
+app.listen(3000);
