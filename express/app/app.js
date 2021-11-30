48cbe75e8dedec27de864adf1ce033b20deb5bec
郁南 <haoyinag0@hotmail.com>
2021-11-30 08:21:18 +0800

    feature: use express

---
+++ express/app/app.js
@@ -0,0 +1,67 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-11-30 08:02:02
+ * @LastEditTime: 2021-11-30 08:11:33
+ * @FilePath: /express/app/app.js
+ * @Description: 
+ */
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const users = require('./users');
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(users);
+});
+
+app.post("/", (req, res) => {
+  res.send("Req a post method");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Req a put method");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Req a delete method");
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ express/app/users.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-11-30 08:09:39
+ * @LastEditTime: 2021-11-30 08:09:40
+ * @FilePath: /express/app/data.js
+ * @Description:
+ */
+module.exports = ["Yunan"];
