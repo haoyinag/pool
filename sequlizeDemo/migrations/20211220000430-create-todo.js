3da4b316d092f8f469ad1307fbbd7d8b1724b33f
郁南 <haoyinag0@hotmail.com>
2021-12-20 08:16:12 +0800

    todo list

---
+++ sequlizeDemo/migrations/20211220000430-create-todo.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: 郁南
+ * @LastEditors: 郁南
+ * @Date: 2021-12-20 08:04:30
+ * @LastEditTime: 2021-12-20 08:08:01
+ * @FilePath: /pool/sequlizeDemo/migrations/20211220000430-create-todo.js
+ * @Description: 
+ */
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      deadline: {
+        type: Sequelize.DATE
+      },
+      content: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Todos');
+  }
+};
